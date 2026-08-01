fad764ca4106ed84fb6620d70f8e2a2aa5b00fe5
Nguyễn Hoàng Quân <nguyenhoangquan7907@gmail.com>
2026-08-02 01:26:21 +0700

    nop bai day5

---
+++ day5/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI(title='library_management ')
+
+library =  {
+    "ten_thu_vien": "Thư viện Rikkei",
+    "dia_chi": "123 Nguyễn Văn Cừ, Hà Nội",
+    "gio_mo_cua": "08:00 - 21:00"
+}
+
+@app.get('/api/v1/library-info')
+def getlibrary():
+    return library
